21733077f29f06036053d803ac13784c2818b6a4
ThroughTheVoid <info_master4@yahoo.com>
2014-10-09 13:36:12 -0400

    Hopefully done with LEVEL
    added draw functions for tiles and items, added placeItem function
    (which I'm not sure if we'll use)

--- level.js
+++ level.js
@@ -22,8 +22,31 @@ function Level(width,height){
 	this.setTile = function(x,y,tile){
 		this.tileMatrix[x][y] = tile;
 	};
+	this.drawTiles = function(){
+		for(var x=0;y<this.width;x++){
+			for(var y=0;y<this.height;y++){
+				var tile = this.tileMatrix[x][y];
+				tile.draw();
+			}
+		}
+	};
 	//item layer
 	this.getContainer = function(x,y){
-		return this.itemMatrix[x][y];
-	}
+		return this.containerMatrix[x][y];
+	};
+	this.placeItem = function(x,y,item){
+		item.moveTo(this.containerMatrix[x][y]);
+	};
+	this.drawItems = function(){
+		for(var x=0;y<this.width;x++){
+			for(var y=0;y<this.height;y++){
+				var container = this.containerMatrix[x][y];
+				for(var i=0;i<container.list.length;i++){
+					var item = container.list[i];
+					item.draw();
+				}
+			}
+		}
+	};
+	
 }
