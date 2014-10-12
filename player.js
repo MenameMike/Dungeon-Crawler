1c53be68161855ea30d4a61f9e33b054acc09958
Michael Newman <newmanmike17@yahoo.com>
2014-10-12 09:57:11 -0400

    quick fix

--- player.js
+++ player.js
@@ -1,13 +1,13 @@
 var Player = function(health, x, y, container){
-this.myInventory = container;
-this.get_MOB = new Mob(100, x, y, this.myInventory );
+this.inventory = container;
+this.get_MOB = new Mob(100, x, y, this.inventory );
 
 	this.strength = 10;
 	this.intelligence = 10;
 	this.agility = 10;
 
 
-	this.inventory = new Container();
+	
 
 	this.draw = function(){
 			
