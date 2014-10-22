84ce1fcac55a2eb2830fde9d83f3bb3d2eedb3c5
ThroughTheVoid <info_master4@yahoo.com>
2014-10-22 11:55:08 -0400

    added border to hud

--- hud.js
+++ hud.js
@@ -1,10 +1,12 @@
+dh.resources.load("border.png");
 function Hud(){
 	this.health = 100;
 	this.name = "myName";
-	this.offsetX = 700;
-	this.offsetY = 0;
+	this.offsetX = 600;
+	this.offsetY = 32;
 	this.draw = function(){
 		g.drawRect(0+this.offsetX,0+this.offsetY,128,600,"#000");
+		g.drawImage(dh.resources.getImage("border.png"),0+this.offsetX-10,0+this.offsetY,100,200);
 		g.drawText("Name: " + this.name,0+this.offsetX,20+this.offsetY,"#fff");
 		g.drawText("HP: " + this.health,0+this.offsetX,20+32+this.offsetY,"#fff");
 		g.drawText("Turn: " + turnCount,0+this.offsetX,20+62+this.offsetY,"#fff");
